ff28a78fee9fea62de91d131bba8cda3593e0214
yusufurur <yusufurur@gmail.com>
2023-02-15 22:42:46 -0500

    Delete spinner2.js

--- spinner2.js
+++
@@ -1,6 +0,0 @@
-const spinner2 = ['|', '/', '-', '\\', '-', '-', '-'];
-for (let i = 0; i < spinner2.length; i++) {
-  setTimeout(() => {
-    process.stdout.write(`\r${spinner2[i]}`);
-  }, i * 200);
-}
